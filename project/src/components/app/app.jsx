26e7cf637d118ddf7dd048ac56ab8888951108d4
ysveshnikov <ysveshnikov@ivi.ru>
2021-06-03 22:29:24 +0300

    Добавляет роутинг

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {arrayOf, bool, number, shape, string} from 'prop-types';
 import Main from '../main/main';
+import SingIn from '../sing-in/sing-in';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
 
 
 function App(props) {
-  return <Main cards={props.cards}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main cards={props.cards}/>
+        </Route>
+        <Route exact path="/login">
+          <SingIn/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
