23825acae91266e3e1e33de23831dc95e0f4e29d
ysveshnikov <ysveshnikov@ivi.ru>
2021-06-20 17:52:32 +0300

    Добавляет редьюсер

--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -4,3 +4,12 @@ const initState = {
   city: '',
   offers,
 };
+
+const reducer = (state = initState, {type, payload}) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export {reducer};
