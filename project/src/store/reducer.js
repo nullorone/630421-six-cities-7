2118ea176fcf6e6acd7de42015385c2ec259a805
ysveshnikov <ysveshnikov@ivi.ru>
2021-06-24 21:20:26 +0300

    Добавляет обработку сортировки

--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -1,30 +1,67 @@
 import Adapter from '../utils/adapter';
 import {offers as mockOffers} from '../mocks/offers';
+import {reviews as mockReviews} from '../mocks/reviews';
 import {ActionType} from './action';
-
-const INIT_CITY_NAME = 'Paris';
-const INIT_OFFERS = new Adapter(mockOffers).normalize();
+import {SortName, INIT_CITY_NAME} from '../utils/const';
 
 const initState = {
   city: INIT_CITY_NAME,
-  offers: INIT_OFFERS.slice().filter((offer) => offer.city.name === INIT_CITY_NAME),
+  initOffers: [],
+  offers: [],
+  reviews: [],
+  cardIdxHover: null,
 };
 
 const reducer = (state = initState, {type, payload}) => {
   switch (type) {
+    case ActionType.GET_OFFERS:
+      return {
+        ...state,
+        offers: [...new Adapter(mockOffers).normalize()],
+        initOffers: [...new Adapter(mockOffers).normalize()],
+      };
+    case ActionType.GET_REVIEWS:
+      return {
+        ...state,
+        reviews: [...new Adapter(mockReviews).normalize()],
+      };
     case ActionType.CHANGE_CITY:
       return {
         ...state,
         city: payload,
       };
-    case ActionType.FILTERED_OFFERS:
+    case ActionType.FILTERED_OFFERS_OF_CITY:
       return {
         ...state,
-        offers: INIT_OFFERS.filter((offer) => offer.city.name === payload),
+        offers: [...state.initOffers].filter((offer) => offer.city.name === payload),
+      };
+    case ActionType.FILTERED_OFFERS_OF_SORT:
+      return {
+        ...state,
+        offers: payload === SortName.popular
+          ? [...state.initOffers].filter((offer) => offer.city.name === state.city)
+          : [...state.offers].sort(getSortOffers(payload)),
+      };
+    case ActionType.SET_CARD_IDX_HOVER:
+      return {
+        ...state,
+        cardIdxHover: payload,
       };
     default:
       return state;
   }
 };
+
+function getSortOffers(sortName) {
+  switch (sortName) {
+    case SortName.top:
+      return (a, b) => b.rating - a.rating;
+    case SortName.toHight:
+      return (a, b) => a.price - b.price;
+    case SortName.toLow:
+      return (a, b) => b.price - a.price;
+    default:
+  }
+}
 
 export {reducer};
