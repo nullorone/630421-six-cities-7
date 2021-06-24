fd7105bd778ecd3180fb12fd062e445562c62033
ysveshnikov <ysveshnikov@ivi.ru>
2021-06-24 21:20:43 +0300

    Выносит константы

---
+++ project/src/utils/const.js
@@ -0,0 +1,8 @@
+export const INIT_CITY_NAME = 'Paris';
+
+export const SortName = {
+  popular: 'Popular',
+  toHight: 'Price: low to high',
+  toLow: 'Price: high to low',
+  top: 'Top rated first',
+};
